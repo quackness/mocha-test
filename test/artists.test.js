1655189aef5e6ddec95eca51477f4f0e909969d9
Karolina Redden <dubajkaro@gmail.com>
2024-02-14 14:10:55 -0400

    add one extra check for the status

--- test/artists.test.js
+++ test/artists.test.js
@@ -3,12 +3,12 @@ const supertest = require('supertest');
 const app = require('../app');
 
 describe('Artists API', () => {
-  it('should get a list of artists', async () => {
-
+  it('should return status 200 upon connection', async () => {
     const response = await supertest(app)
       .get('/api/artists');
-
+    console.log(response)
     assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.header['content-type'], 'application/json; charset=utf-8');
 
   });
 });
