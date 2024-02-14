da96eecdb8c513ea57c3a0e8c978ab8ae9052a17
Karolina Redden <dubajkaro@gmail.com>
2024-02-13 22:21:06 -0400

    add a successful test of a route

--- test/artists.test.js
+++ test/artists.test.js
@@ -1,3 +1,18 @@
 const assert = require('assert');
 const supertest = require('supertest');
-const app = require('../routes/artists/index.js');
+// const expect = require('chai');
+// const app = require('../routes/artists/index');
+const app = require('../app');
+// console.log(app)
+
+describe('Artists API', () => {
+  it('should get a list of artists', async () => {
+
+    const response = await supertest(app)
+      .get('/api/artists');
+
+    assert.strictEqual(response.status, 200);
+
+  });
+});
+
